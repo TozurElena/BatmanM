057c91af3131d7f0f2a8a1ff4516bb35fb08aaad
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-02-20 17:55:57 +0100

    add slider photo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+  loop: true,
+  spaceBetween:20,
+  slidesPerView: 3,
+  centeredSlides: true
+});
+
+sliderThumbs.on('click', (swiper) => {
+  swiper.slideTo(swiper.clickedIndex)
+})
+
+const sliderMain = new Swiper('.slider-main', {
+  // beskonechno
+  loop: true,
+  // sviazali c drugim sliderom
+  thumbs:{
+    swiper: sliderThumbs
+  },
+  
+});
